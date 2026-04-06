04597ed7029bc7d87e99d55c9a2c171e1357401f
ushasri-2588 <satyaushasrilakshmimallajosyul@gmail.com>
2026-04-06 22:39:20 +0530

    Update Conditional Operator.c

--- Conditional Operator.c
+++ Conditional Operator.c
@@ -6,5 +6,6 @@ printf("Enter three numbers: ");
 scanf("%d %d %d", &a, &b, &c);
 max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
 printf("Maximum number = %d", max);
+printf("\n     25331A05G9");
 return 0;
 }
