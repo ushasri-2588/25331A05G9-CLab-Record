6777a7f25194e9fff3e26a4ce5430a6be2759196
ushasri-2588 <satyaushasrilakshmimallajosyul@gmail.com>
2026-04-12 23:37:14 +0530

    Create Week8-1.c

---
+++ Week8-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int sum(int a,int b) 
+{
+    return a+b;
+}
+int main()
+{
+    int x,y;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&x,&y);
+    printf("Sum = %d",sum(x,y));
+    printf("\n     25331A05I3");
+    return 0;
+}
